e29bf2b8e196a3f2f1d6b2f01f6578ffe4b7875b
Tim <tetchel@gmail.com>
2017-06-14 21:28:25 -0400

    Remove tablequiz

--- tablequiz/js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-window.onbeforeunload = function() {
-    if(isEmptyObject(answersArray)) {
-        return "Are you sure you want to leave? Your current quiz will be lost!";
-    }
-    return;
-}
